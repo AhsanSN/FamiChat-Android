a5b127440437238538eb358b32609dd7e500b268
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-07-13 11:59:57 +0500

    removing rubbish

--- www/js/app.js
+++ www/js/app.js
@@ -8,13 +8,6 @@ angular.module('homeMenu', ['ionic'])
     ];
 
     // Create and load the Modal
-    //modal in imported from html
-    $ionicModal.fromTemplateUrl('new-task.html', function (modal) {
-      $scope.taskModal = modal;
-    }, {
-        scope: $scope,
-        animation: 'slide-in-up'
-      });
 
     // Called when the form is submitted
     $scope.createTask = function (task) {

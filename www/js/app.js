7ebe5dca026a1dbb1e67f3cdafdf0b08cb1154bb
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-07-14 16:53:19 +0500

    modal added

--- www/js/app.js
+++ www/js/app.js
@@ -2,14 +2,31 @@ angular.module('homeMenu', ['ionic'])
 
   .controller('homeMenuCtrl', function ($scope, $ionicModal) {
 
+    $ionicModal.fromTemplateUrl('chatModal.html', function (modal) {
+      $scope.taskModal = modal;
+    }, {
+        scope: $scope,
+        animation: 'slide-in-up'
+    });
+
     // quit app
     $scope.quitApp = function () {
       console.log("quit")
     };
 
+    $scope.toChatModal = function () {
+      console.log("tochat modal")
+      $scope.taskModal.show();
+    }
+
     $scope.toChat = function () {
       console.log("tochat")
       window.location = "/pages/chat.html?room=7860"
+    }
+
+    $scope.closeChatModal = function () {
+      console.log("hide modal")
+      $scope.taskModal.hide();
     }
 
     $scope.createRoom = function () {
@@ -21,5 +38,7 @@ angular.module('homeMenu', ['ionic'])
       console.log("JoinRoom")
       window.location = "/pages/chat.html?room=7860"
     }
+
+    
 
   })

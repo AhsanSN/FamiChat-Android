e908b8162d5bc84781401cc7d77327eff4f60be6
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-07-14 09:56:31 +0500

    quit added

--- www/js/app.js
+++ www/js/app.js
@@ -1,2 +1,16 @@
 angular.module('homeMenu', ['ionic'])
 
+  .controller('homeMenuCtrl', function ($scope, $ionicModal) {
+
+    //used as database for your data
+    $scope.tasks = [
+      { title: 'My task 1' }
+    ];
+
+    // quit app
+    $scope.quitApp = function () {
+      console.log("quit")
+    };
+
+
+  })

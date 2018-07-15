c95727d96ac1a2bc5ad7c290378bd7d76f69a8c5
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-07-15 22:12:46 +0500

    adding joinChat

--- www/js/app.js
+++ www/js/app.js
@@ -2,8 +2,11 @@ angular.module('homeMenu', ['ionic'])
 
   .controller('homeMenuCtrl', function ($scope, $ionicModal) {
 
+    //my rooms
+
     $ionicModal.fromTemplateUrl('chatModal.html', function (modal) {
       $scope.taskModal = modal;
+      console.log("chatModal.html loaded")
     }, {
         scope: $scope,
         animation: 'slide-in-up'
@@ -33,12 +36,35 @@ angular.module('homeMenu', ['ionic'])
       console.log("createRoom")
       window.location = "/pages/chat.html?room=7860"
     }
-    
-    $scope.JoinRoom = function () {
-      console.log("JoinRoom")
-      window.location = "/pages/chat.html?room=7860"
+       
+  })
+
+  .controller('joinRoomCtrl', function ($scopeJoinRoom, $ionicModal) {
+
+    $ionicModal.fromTemplateUrl('JoinRoom.html', function (modal) {
+            console.log("JoinRoom")
+
+    $scopeJoinRoom.taskModal = modal;
+    }, {
+        scope: $scopeJoinRoom,
+        animation: 'slide-in-up'
+    });
+
+    $scopeJoinRoom.joinRoom = function () {
+      console.log("JoinRoom.html loaded")
+      $scopeJoinRoom.taskModal.show();
     }
 
-    
+
 
   })
+
+
+
+
+
+
+
+
+
+
